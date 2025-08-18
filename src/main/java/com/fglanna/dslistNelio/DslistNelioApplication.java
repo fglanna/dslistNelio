90ed7695a071c5d55970c2217cc30d168d87ef66
FLAVIO GOMES DE LANNA <132150354+fglanna@users.noreply.github.com>
2025-08-18 13:00:56 -0300

    Project created

---
+++ src/main/java/com/fglanna/dslistNelio/DslistNelioApplication.java
@@ -0,0 +1,13 @@
+package com.fglanna.dslistNelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistNelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistNelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fglanna/dslistNelio/DslistNelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fglanna.dslistNelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistNelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
