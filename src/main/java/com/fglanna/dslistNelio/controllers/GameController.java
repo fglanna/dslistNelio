b3c06fbde3323b14077bc704bd1b092d20cdeab0
FLAVIO GOMES DE LANNA <132150354+fglanna@users.noreply.github.com>
2025-08-19 15:30:59 -0300

    GET game by id

--- src/main/java/com/fglanna/dslistNelio/controllers/GameController.java
+++ src/main/java/com/fglanna/dslistNelio/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fglanna.dslistNelio.dto.GameDTO;
 import com.fglanna.dslistNelio.dto.GameMinDTO;
 import com.fglanna.dslistNelio.services.GameService;
 
@@ -18,12 +20,19 @@ public class GameController {
 	private GameService gameService;
 	
 	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {		
+		GameDTO result = gameService.findById(id);
+		return result;
+		 
+	}
+	
 	@GetMapping
 	public List<GameMinDTO> findAll() {		
 		List<GameMinDTO> result = gameService.findAll();
 		return result;
-		 
-	 }
+		
+	}
 	
 
 }
