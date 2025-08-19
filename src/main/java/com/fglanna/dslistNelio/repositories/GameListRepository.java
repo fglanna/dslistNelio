bc9ddd603e875b72d631a8925d16aed0cbf8c378
FLAVIO GOMES DE LANNA <132150354+fglanna@users.noreply.github.com>
2025-08-19 18:42:22 -0300

    GET lists

---
+++ src/main/java/com/fglanna/dslistNelio/repositories/GameListRepository.java
@@ -0,0 +1,10 @@
+package com.fglanna.dslistNelio.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fglanna.dslistNelio.entities.GameList;
+
+
+public interface GameListRepository extends JpaRepository<GameList, Long> {
+
+}
---
+++ src/main/java/com/fglanna/dslistNelio/services/GameListService.java
@@ -0,0 +1,29 @@
+package com.fglanna.dslistNelio.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fglanna.dslistNelio.dto.GameListDTO;
+import com.fglanna.dslistNelio.entities.GameList;
+import com.fglanna.dslistNelio.repositories.GameListRepository;
+
+
+@Service
+public class GameListService {
+	
+	@Autowired
+	private GameListRepository gameListRepository;
+	
+	
+	@Transactional(readOnly = true)
+	public List<GameListDTO> findAll() {		
+		List<GameList> result = gameListRepository.findAll();
+		List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
+		return dto;
+		
+	}
+
+}
